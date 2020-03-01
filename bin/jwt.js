48d714f9425d2c6a2a2cca0d7397d40b460dfae4
Pedro Soares <hspedro@profusion.mobi>
2020-03-01 19:10:34 -0300

    feat: add binary parsing algorithm, secret and payload
    To check all the options available, run as:
    `node bin/jwt.js -h`

---
+++ bin/jwt.js
@@ -0,0 +1,46 @@
+const yargs = require('yargs');
+const uuidv4 = require('uuid/v4');
+
+// eslint-disable-next-line no-unused-vars
+const { argv } = yargs
+  .option('algorithm', {
+    alias: 'a',
+    description: `Algorithm to sign JWT, use one of the following: [
+      "HS256", "HS384", "HS512",
+      "RS256", "RS384", "RS512"
+      "ES256", "ES384", "ES512",
+      "PS256", "PS384", "PS512",
+      "none"
+    ];
+`,
+    type: 'string',
+  })
+  .option('secret', {
+    alias: 's',
+    description: 'Secret Key to be used on creation JWT token',
+    type: 'string',
+  })
+  .option('payload', {
+    alias: 'p',
+    default: '',
+    description: `Payload to be encoded with JWT. This is a string that will be
+    parsed as JSON`,
+    type: 'string',
+  })
+  .help()
+  .alias('help', 'h');
+
+// eslint-disable-next-line no-unused-vars
+const parsePayload = async input => {
+  let parsed = '';
+  try {
+    parsed = await JSON.parse(input);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('Error while generating payload: ', e);
+  }
+  return parsed;
+};
+
+// eslint-disable-next-line no-unused-vars
+const parseSecret = secret => secret || uuidv4();
